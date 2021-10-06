cd03c5e08273ee10b590781cbadd73e00d274d5f
NiumXp <formigacomcaimbra@gmail.com>
2021-10-05 22:06:43 -0300

    add errors, ModelMeta and rename _attribute class

--- spl/__init__.py
+++ spl/__init__.py
@@ -1,35 +1,60 @@
 import typing
 
-T = typing.TypeVar('T', bound="Model")
+_NULL = object()
 
-
-class _attribute:
+class Attribute:
     __slots__ = ()
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        default: typing.Any = _NULL,
+    ) -> None:
+        self._default = default
+
+    @property
+    def default(self) -> typing.Any:
+        if callable(self._default):
+            return self._default()
+
+        return self._default
 
     def __get__(self, *_) -> typing.Any:
         ...
 
     def __set__(*_) -> None:
-        raise AttributeError("you can't set this attribute")
+        raise AttributeError("you can't set an Model attribute, "
+                             "use an Entry to edit a Model instance")
 
 
-class Model:
-    __models__: typing.ClassVar[dict[str, type[T]]] = {}
+class ModelMeta(type):
+    def __len__(self) -> int:
+        return 0
 
-    def __init_subclass__(cls, **options) -> None:
+
+class Model(metaclass=ModelMeta):
+    __models__: typing.ClassVar[dict[str, type["Model"]]] = {}
+
+    def __init_subclass__(cls, **options: typing.Any) -> None:
         self: type[Model] = Model
 
-        if cls.__base__ != self:
+        if cls.__base__ != self:  # type: ignore
             raise RuntimeError("you can't subclass a Model")
 
         name = options.pop("name", cls.__name__)
         self.__models__[name] = cls
 
         for identifier, annotation in cls.__annotations__.items():
-            ...
+            error = None
 
-            descriptor = _attribute()
+            if isinstance(annotation, str):
+                error = "str annotations is not supported "
+            elif annotation not in {str, int, float}:
+                error = f"{annotation!r} is not a supported annotation"
+
+            if error:
+                raise RuntimeError(f"{cls.__name__}.{identifier}: {error}")
+
+            default = getattr(cls, identifier, _NULL)
+            descriptor = Attribute(default)
+
             setattr(cls, identifier, descriptor)
